0834dad12a7331f09ee7a439ec600b9da22b0093
noMoon <nomoonx@gmail.com>
2014-11-09 04:10:08 -0500

    demo for JML

---
+++ ProjectTwo/src/demo.java
@@ -0,0 +1,24 @@
+/**
+ * 类demo.java的实现描述：TODO 类实现描述
+ * 
+ * @author noMoon Nov 9, 2014 3:10:37 AM
+ */
+public class demo {
+
+    public static void main(String[] args) {
+	System.out.println(greaterThanZero(0));
+    }
+
+    // @ requires num>0;
+    // @ ensures \result == true;
+    // @ also
+    // @ requires num<=0;
+    // @ ensures \result == false;
+    public static boolean greaterThanZero(int num) {
+	if (num > 0)
+	    return true;
+	else
+	    return false;
+    }
+
+}
